17e84d446805a438d7b60655a717376332618220
Vitor <vitorprofissionalpp10@gmail.com>
2026-03-12 17:58:13 -0300

    Auto commit on 2026-03-12 17:58:13

--- controllers/CadastroController.cc
+++ controllers/CadastroController.cc
@@ -1,6 +1,4 @@
 #include "CadastroController.h"
-
-#include <cmath>
 #include<string>
 
 void CadastroController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
@@ -16,15 +14,16 @@ void CadastroController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::
         return;
     }
 
-<<<<<<< HEAD
     std::string clientName = (*receive_json)["clientes"].asString();
 
     auto client = drogon::app().getDbClient();
     std::string sql_command = "INSERT INTO clientes (nome) VALUES ($1)";
 
-    client->execSqlAsync(
+    client->execSqlAsync
+    (
     sql_command,
-    [callback](const drogon::orm::Result &db_result) {
+    [callback](const drogon::orm::Result &db_result)
+    {
         Json::Value successJson;
         successJson["status"] = "success";
         successJson["message"] = "Agendado no Banco";
@@ -36,7 +35,8 @@ void CadastroController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::
 
         callback(resp);
     },
-    [callback](const drogon::orm::DrogonDbException &db_error) {
+    [callback](const drogon::orm::DrogonDbException &db_error)
+    {
         Json::Value errorJson;
         errorJson["status"] = "failed";
         errorJson["message"] = "Erro no banco";
@@ -49,38 +49,5 @@ void CadastroController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::
         callback(resp);
     },
     clientName
-);
-=======
-    std::string clientName = (*receive_json)["clientName"].asString();
-
-    auto client = drogon::app().getDbClient();
-    std::string sql_command = "INSERT INTO clientes (nome) VALUES ($1)";
->>>>>>> 8dcaf7bff30cc90900c955a841a0d643da597a99
-
-	client->execSqlAsync(
-		sql_command,
-		[callback](const drogon::orm::Result &db_result) {
-			Json::Value success_json;
-			success_json["status"] = "success";
-			success_json["message"] = "Agendado no banco";
-			
-			auto resp = drogon::HttpResponse::newHttpJsonResponse(success_json);
-			resp->addHeader("Access-Control-Allow-Origin", "*");
-			resp->addHeader("Access-Control-Allow-Methods", "*");
-			resp->addHeader("Access-Control-Allow-Headers", "*");
-			callback(resp);
-		},
-		[callback](const drogon::orm::DrogonDbException &db_error) {
-		    Json::Value error_json;
-		    error_json["status"] = "failed";
-		    error_json["message"] = "Erro no banco";
-		    
-		    auto resp = drogon::HttpResponse::newHttpJsonResponse(error_json);
-		    resp->addHeader("Access-Control-Allow-Origin", "*");
-		    resp->addHeader("Access-Control-Allow-Methods", "*");
-		    resp->addHeader("Access-Control-Allow-Headers", "*");
-		    callback(resp);
-		},
-		clientName
-	);
+    );
 }
